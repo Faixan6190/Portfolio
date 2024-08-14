05bd54789d28a08a88cf807a2a16fc48a996f725
Faixan6190 <faixan6190@gmail.com>
2024-08-15 00:24:10 +0500

    add about.jsx file

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,6 @@
 import { HERO_CONTENT } from "../constants/index";
+import profilePic from "../assets/faizanProfile.jpg";
+
 const Hero = () => {
   return (
     <div className="border-b border-neutral-900 pb-4 lg:mb-35">
@@ -9,7 +11,12 @@ const Hero = () => {
             <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
               Full Stack Developer
             </span>
-            <p>{HERO_CONTENT}</p>
+            <p className="my-2 max-w-xl py-6 font-light tracking-tighter">{HERO_CONTENT}</p>
+          </div>
+        </div>
+        <div className="w-full lg:w-1/2 lg:p-8">
+          <div className="flex justify-center">
+            <img src={profilePic} alt="Faizan Ahmed" width={400} height={400} className="rounded-xl" />
           </div>
         </div>
       </div>
