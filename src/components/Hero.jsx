b046c1d161af44874e1e23c8f618c584eb2411a5
Faixan6190 <faixan6190@gmail.com>
2024-08-15 00:54:10 +0500

    add technologies.jsx file

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-6xl">Muhammad Faizan Ahmed</h1>
+            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-6xl sm:text-center">Muhammad Faizan Ahmed</h1>
             <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
               Full Stack Developer
             </span>
@@ -16,7 +16,7 @@ const Hero = () => {
         </div>
         <div className="w-full lg:w-1/2 lg:p-8">
           <div className="flex justify-center">
-            <img src={profilePic} alt="Faizan Ahmed" width={400} height={400} className="rounded-xl" />
+            <img src={profilePic} alt="Faizan Ahmed" width={400} height={400} className="rounded-2xl" />
           </div>
         </div>
       </div>
