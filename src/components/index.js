76ef68e2b97666791c2dc921a7c482d9bc7daf25
Faixan6190 <faixan6190@gmail.com>
2024-08-15 21:20:16 +0500

    add file experience

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Navbar from "./Navbar";
 import Hero from "./Hero";
 import About from "./About";
 import Technologies from "./Technologies";
+import Experience from "./Experience";
 
-export { Navbar, Hero, About, Technologies };
+export { Navbar, Hero, About, Technologies, Experience };
